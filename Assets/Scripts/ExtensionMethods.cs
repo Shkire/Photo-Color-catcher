abd9585162c15be3bd1ffbd8b1f4dd332c6c06a1
Shkire <danielsmash94@hotmail.com>
2016-01-21 19:53:10 +0100

    ExtensionMethods for RigidBody and MapSchedule added

---
+++ Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ExtensionMethods {
+
+    public static void ResetForces(this Rigidbody rb) {
+
+        rb.isKinematic = true;
+        rb.isKinematic = false;
+
+    }
+
+    public static void ResetForces(this Rigidbody2D rb)
+    {
+
+        rb.isKinematic = true;
+        rb.isKinematic = false;
+
+    }
+
+    public static void ResetAndAddForce(this Rigidbody rb, Vector3 force)
+    {
+
+        rb.ResetForces();
+        rb.AddForce(force);
+
+    }
+
+    public static void ResetAndAddForce(this Rigidbody2D rb, Vector3 force)
+    {
+
+        rb.ResetForces();
+        rb.AddForce(force);
+
+    }
+
+}
---
+++ Assets/Scripts/MapSchedule.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapSchedule {
+
+    public enum CellContent
+    {
+        Player,
+        Platform,
+        Flag,
+        Camera,
+        DecorationUp,
+        DecorationDown,
+        DecorationLeft,
+        DecorationRigth,
+        DecorationHoriz,
+        DecorationVert,
+        EnemySpawn
+    }
+
+    private int size;
+
+    private List<CellContent>[,] schedule;
+
+    public int mapSize
+    {
+        get
+        {
+            return size;
+        }
+        set
+        {
+            size = value;
+        }
+    }
+
+    public void Config()
+    {
+
+        schedule = new List<CellContent>[size, size];
+
+    }
+
+}
