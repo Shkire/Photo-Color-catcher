fff7b316973117ee8e5df59902870ff627acfb48
Daniel Villaverde <danielsmash94@hotmail.com>
2016-02-03 18:02:23 +0100

    PjMovementController fixed

---
+++ Assets/Scripts/PjMovementController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PjMovementController : CharacterMovementController {
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		float movement = Input.GetAxis ("Horizontal") * speed;
+		Move (movement);
+		float canJump = Input.GetAxis ("Vertical");
+		if (canJump > 0)
+			Jump ();
+	}
+
+
+
+}
