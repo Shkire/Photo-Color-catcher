a9a75e4a9a4864837bb71441db796a9c22d298f7
Daniel Villaverde <danielsmash94@hotmail.com>
2016-02-03 14:27:38 +0100

    EnemyMovementController created

---
+++ Assets/Scripts/EnemyMovementController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovementController : CharacterMovementController {
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	
+	}
+}
