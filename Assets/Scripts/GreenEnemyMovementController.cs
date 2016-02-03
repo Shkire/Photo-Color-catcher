59bb3302fbdc5f111c0f26441dbfc2f995b4709f
Daniel Villaverde <danielsmash94@hotmail.com>
2016-02-03 18:00:02 +0100

    ObjectiveDetectionController added
    PjMovementController fixing.
    GreenEnemyMovementController added too

---
+++ Assets/Scripts/GreenEnemyMovementController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GreenEnemyMovementController : EnemyMovementController {
+
+	void FixedUpdate(){
+
+	}
+
+
+
+}
---
+++ Assets/Scripts/ObjectiveDetectionController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectiveDetectionController : MonoBehaviour {
+
+	[SerializeField]
+	private float sightRange;
+
+	[SerializeField]
+	private string tag;
+
+	void FixedUpdate(){
+
+		RaycastHit2D[] hits = Physics2D.Raycast (this.gameObject.transform.position, Vector2.right, sightRange);
+
+
+	}
+
+}
