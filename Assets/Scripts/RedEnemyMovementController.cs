c45ba26310c5882ae34ce98dd5f62a8232fa9ce9
Jesuskfc <gsus1946@gmail.com>
2016-02-08 20:21:07 +0100

    RedEnemyMovementController

---
+++ Assets/Scripts/RedEnemyMovementController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RedEnemyMovementController : EnemyMovementController {
+
+	[SerializeField]
+	private float maxResponseTime;
+
+	[SerializeField]
+	private float minResponseTime;
+
+	[SerializeField]
+	private GameObject pJ;
+
+	private float leftResponseTime;
+
+	private movementType nowMovingTo;
+
+	private System.Random rnd;
+
+
+
+
+	void OnEnable(){
+		rnd = new System.Random (Guid.NewGuid ().GetHashCode ());
+	}
+
+
+
+
+	void FixedUpdate(){
+
+		NormalMovement ();
+
+	}
+
+	protected override void NormalMovement(){
+
+		float pos = pJ.transform.position.x;
+
+		if (pos > this.transform.position.x)
+			Move (1);
+		if (pos < this.transform.position.x)
+			Move (-1);
+
+	}
+
+
+
+
+
+
+}
