5e410d5f675ecdb5189ca90dddc7e33b5f2bd4a9
Jesuskfc <gsus1946@gmail.com>
2016-02-16 20:46:56 +0100

    PjAttackEnemy Added || Sprite Frame

---
+++ Assets/Scripts/PjAttackController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PjAttackController : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject frame;
+
+	private bool active;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (active) {
+			frame.GetComponent<Collider2D> ().enabled = false;
+			active = false;
+		}
+		
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			frame.GetComponent<SpriteRenderer> ().enabled = true;
+
+		}
+
+
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			frame.GetComponent<Collider2D> ().enabled = true;
+			frame.GetComponent<SpriteRenderer> ().enabled = false;
+			active = true;
+		}
+
+
+
+	}
+
+
+
+
+
+}
