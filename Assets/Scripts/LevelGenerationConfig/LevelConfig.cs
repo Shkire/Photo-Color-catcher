094b9252483cef1fee780a23385f62c2311ae423
Daniel Villaverde <danielsmash94@hotmail.com>
2016-05-02 21:02:28 +0200

    LevelConfig asset created

---
+++ Assets/Scripts/LevelGenerationConfig/LevelConfig.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class LevelConfig {
+
+	public int level;
+	public int teleportMin;
+	public int teleportMax;
+	public int maxTeleportNumBySide;
+
+}
