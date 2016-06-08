b1b177fc2d054207296f47765db39e02470ac007
Daniel Villaverde <danielsmash94@hotmail.com>
2016-06-08 21:11:03 +0200

    Useless lines deleted

--- Assets/Scripts/ImageProcessing/ImgProcessManager.cs
+++ Assets/Scripts/ImageProcessing/ImgProcessManager.cs
@@ -54,59 +54,7 @@ public class ImgProcessManager : Singleton<ImgProcessManager> {
 
 	void Start () {
 		PersistenceManager.MainLoad ();
-		//ProccesAndIndexImage (imgPath);
-		//PersistenceManager.LoadImageList ();
-		//images = new List<ProcessedImage> ();
-		//LoadImage (imgPath);
-		//Debug.Log ("Dividing image");
-		//DivideImage (0);
-		//foreach (int index in PersistenceManager.GetAllIds())
-			//ImgProcessManager.Instance.Instantiate (index, "Creada");
-		//PersistenceManager.SaveImage (images[0]);
-		//imagen = new ProcessedImage (imgPath);
-		//StartCoroutine (imagen.InitProcessedImage(this.gameObject));
 	}
-	/*
-	void FixedUpdate(){
-		StartCoroutine (imagen.ToTexture2D (texturita));
-	}
-	*/
-	// Update is called once per frame
-	/*
-	void FixedUpdate () {
-		if ((Input.GetKey(KeyCode.Q)) && count < imagen.width + imagen.height) 
-		{
-			imagen.SetRGBValue (255, 255, 255, count / imagen.width, count % imagen.width);
-			count++;
-		}
-		sprite.sprite = Sprite.Create(imagen.ToTexture2D(),new Rect(0,0,10,10),new Vector2(0,0));
-	}
-	*/
-
-	/*
-	private IEnumerator InitImageProcessing()
-	{
-		imagen=new ProcessedImage (Application.persistentDataPath+"/"+imgPath);
-		sprite.sprite = Sprite.Create(imagen.ToTexture2D(),new Rect(0,0,10,10),new Vector2(0,0));
-		yield return null;
-	}
-	*/
-
-	/*
-	void LoadImage(string path)
-	{
-		images.Add (new ProcessedImage (path));
-	}
-	*/
-
-	/*
-	void DivideImage (int idx)
-	{
-		if (images [idx] != null)
-			//StartCoroutine(images [idx].Divide (divisionFactor));
-			images[idx].Divide(divisionFactor);
-	}
-	*/
 
 	void DidImageInit()
 	{
